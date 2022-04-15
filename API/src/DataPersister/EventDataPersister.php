2b7d44920f38fdaa1763a55911ce97ee0d8ee4a1
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-04-15 12:47:43 +0300

    create and configure the EventDataPersister

---
+++ API/src/DataPersister/EventDataPersister.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Entity\Event;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class EventDataPersister implements ContextAwareDataPersisterInterface {
+
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+    
+    public function supports($data, array $context = []): bool 
+    {
+        return $data instanceof Event;
+    }
+
+    public function persist($data, array $context = []) {
+
+        $data->setNumEvent('E_'.date_format($data->getDateEvent(), 'dmY'));
+        
+        $this->em->persist($data);
+        $this->em->flush();
+
+        return $data;
+    }
+
+    public function remove($data, array $context = []) {
+
+    }
+}
