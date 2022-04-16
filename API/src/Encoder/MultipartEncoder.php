ac5da9eeca758faab06a319bbfe57d5c7ec5f834
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-04-16 16:03:02 +0300

    remove comments

--- API/src/Encoder/MultipartEncoder.php
+++ API/src/Encoder/MultipartEncoder.php
@@ -1,20 +1,16 @@
 <?php
-// api/src/Encoder/MultipartDecoder.php
 
 namespace App\Encoder;
 
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Serializer\Encoder\DecoderInterface;
 
-final class MultipartDecoder implements DecoderInterface
+final class MultipartEncoder implements DecoderInterface
 {
     public const FORMAT = 'multipart';
 
     public function __construct(private RequestStack $requestStack) {}
 
-    /**
-    * {@inheritdoc}
-    */
     public function decode(string $data, string $format, array $context = []): ?array
     {
         $request = $this->requestStack->getCurrentRequest();
@@ -31,9 +27,6 @@ final class MultipartDecoder implements DecoderInterface
         }, $request->request->all()) + $request->files->all();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function supportsDecoding(string $format): bool
     {
         return self::FORMAT === $format;
