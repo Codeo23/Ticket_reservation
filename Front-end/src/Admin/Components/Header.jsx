5e27a5e58c5389e516ca5781ea1affee3950cbce
Safidy <robustemmanuel@gmail.com>
2022-04-20 10:11:57 +0300

    qlq config admin

---
+++ Front-end/src/Admin/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div className='bg-gradient-to-br from-red-700 to-red-800 text-white py-3 px-5'>
+            <span className='text-3xl font-bold'>E-ticket</span>
+        </div>
+    );
+};
+
+export default Header;
--- Front-end/src/Admin/Pages/Dashboard.js
+++ Front-end/src/Admin/Pages/Dashboard.js
@@ -1,12 +1,38 @@
 import React from "react";
+import Header from "../Components/Header";
 import Navigation from "../Components/Navigation";
+import "../admin.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-regular-svg-icons";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 const Dashboard = (props) => {
   return (
     <div className="flex flex-row max-h-screen">
       <Navigation />
       <div className="grow w-10/12">
-        <h1>Dashboard</h1>
+        <Header />
+        <div className="p-3 flex gap-x-4 justify-center">
+          <div className="bg-slate-500 p-5 grow w-1/12 text-slate-100 rounded-xl">
+            <FontAwesomeIcon icon={faCog} className="text-3xl" />
+            <span className="text-lg">Personnaliser le Ticket</span>
+          </div>
+          <div className="bg-slate-500 p-5 grow w-1/12 text-slate-100 rounded-xl">
+            <FontAwesomeIcon icon={faCog} className="text-3xl" />
+            <span className="text-lg">Personnaliser le Ticket</span>
+          </div>
+          <div className="bg-slate-500 p-5 grow w-1/12 text-slate-100 rounded-xl">
+            <FontAwesomeIcon icon={faCog} className="text-3xl" />
+            <span className="text-lg">Personnaliser le Ticket</span>
+          </div>
+          <div className="bg-slate-500 p-5 grow w-1/12 text-slate-100 rounded-xl">
+            <FontAwesomeIcon icon={faCog} className="text-3xl" />
+            <span className="text-lg">Personnaliser le Ticket</span>
+          </div>
+        </div>
+        <div>
+          <h2>Chiffres d'affaires</h2>
+        </div>
       </div>
     </div>
   );
