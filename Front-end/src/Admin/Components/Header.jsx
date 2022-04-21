d313a8f570923a6abb58588bcdab2110573b4684
Safidy <robustemmanuel@gmail.com>
2022-04-21 17:17:00 +0300

    Debut post données

--- Front-end/src/Admin/Components/Header.jsx
+++ Front-end/src/Admin/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <div className='bg-gradient-to-br from-red-700 to-red-800 text-white py-3 px-5'>
+        <div className='bg-red-900 text-white py-3 px-5'>
             <span className='text-3xl font-bold'>E-ticket</span>
         </div>
     );
--- Front-end/src/actions/event.action.js
+++ Front-end/src/actions/event.action.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_EVENTS = "GET_EVENTS";
+export const ADD_EVENT = "ADD_EVENT";
 
 export const getEvents = () => {
   return (dispatch) => {
@@ -8,6 +9,17 @@ export const getEvents = () => {
       .get("http://localhost:8000/api/events?_sort=date_event&_order=desc")
       .then((events) => {
         dispatch({ type: GET_EVENTS, payload: events.data });
+      })
+      .catch((erreur) => console.log(erreur));
+  };
+};
+
+export const addEvent = (event) => {
+  return (dispatch) => {
+    return axios
+      .post("http://localhost:8000/api/events",event)
+      .then((events) => {
+        dispatch({ type: ADD_EVENT, payload: event });
       })
       .catch((erreur) => console.log(erreur));
   };
