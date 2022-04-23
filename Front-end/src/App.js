10a482d027993a0b57954e39fd07624e9529a641
hasina821 <rmnarry.mr@gmail.com>
2022-04-23 02:10:12 +0200

    Add-auth

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Dashboard from './Admin/Pages/Dashboard';
 import Evenement from './Admin/Pages/Evenement';
 import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/signin' element={<Signin/>}/>
+                <Route path='/signup' element={<Signup/>}/>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/admin' element={<Dashboard/>}/>
                 <Route path='/evenement' element={<Evenement/>}/>
---
+++ Front-end/src/components/form/signupForm.jsx
@@ -0,0 +1,15 @@
+import React,{useState,useEffect, Fragment} from 'react'
+
+
+const SignupForm=()=>{
+    return(
+        <Fragment>
+            <div>
+                this is the signup form
+            </div>
+        </Fragment>
+    )
+
+}
+
+export default SignupForm;
