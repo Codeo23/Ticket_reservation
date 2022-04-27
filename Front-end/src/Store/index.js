e3e834a6ffecb8566c74d2ed784eaee5d1e5345e
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-04-27 20:49:41 +0300

    get the latest version from master

--- Front-end/src/Store/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import eventReducer from "./event.reducer";
-
-export default combineReducers({
-    eventReducer,
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-Un application web pour une reservation de place dans un cinema
