9d268631a9ebe37179741c267dbf7fc5139d0278
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-04-28 18:34:27 +0300

    configure jwt

--- API/config/bundles.php
+++ API/config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
