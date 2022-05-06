90874e048f926021be09bf43ebaa9d8f9b7a1564
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-06 16:41:19 +0300

    configure the service for sending an email

---
+++ API/src/Service/SendInformationEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class SendInformationEmail {
+
+    public function __construct(MailerInterface $mailer)
+    {
+    }
+
+    public function send(string $email){
+        $info = (new Email())
+            ->from('noreply@gmail.com')
+            ->to($email)
+            ->subject('Evènement reporté à une date ultérieur')
+            ->text("Nous sommes navré de vous apprendre que l'évènement a été reporté.")
+        ;
+        $this->mailer->send($info);
+    }
+}
