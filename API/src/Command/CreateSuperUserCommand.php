3d0325c36149e023f7056e2f75e2eb7915f352c4
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-14 17:38:45 +0300

    configure the command

--- API/src/Command/CreateSuperUserCommand.php
+++ API/src/Command/CreateSuperUserCommand.php
@@ -11,34 +11,23 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'CreateSuperUser',
-    description: 'Add a short description for your command',
+    name: 'create:super-user',
+    description: 'Create a super user',
+    aliases: ['create:super-user'],
 )]
 class CreateSuperUserCommand extends Command
 {
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setHelp('Help you to create a super user')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
+        
         return Command::SUCCESS;
     }
 }
